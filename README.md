49d65601174be951fcfd1a944441440a0bc36646
Andreas Harth <andreas.harth@fau.de>
2021-10-26 17:57:38 +0200

    output csv

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The small Python script checks ZIP files with papers in PDF format for the mention of the term "web".
 
-The script excludes mention of the string "The Web Conference 2022" or "2022 World Wide Web Conference", which is part of the template.
+The script excludes mention of the string "The Web Conference 2022" or "2022 World Wide Web Conference" or "WWW '22" or "WWW, FR", which is part of the template.
 
 The check includes the references of the papers, i.e., papers with zero hits do not cite any papers from The Web Conference.
 
@@ -23,7 +23,3 @@ On Debian-based systems, do:
 Simple mode:
 
 ````$ python3 twc.py {ZIP archives}````
-
-or verbose mode to print surrounding words of single mentions:
-
-````$ python3 twc.py -v {ZIP archives}````
--- twc.py
+++ twc.py
@@ -17,6 +17,7 @@ def tokenise(text):
 import sys
 import zipfile
 import os
+import re
 from collections import Counter
 
 if len(sys.argv) <= 1:
@@ -39,19 +40,45 @@ for i in range(index, len(sys.argv)):
             with zip.open(pname) as pfile:
                 try:
                     pstring = get_paper(pfile)
-                    # remove mentions of The Web Conference 2022
+                    # remove newlines
                     pstring = pstring.replace('\r', ' ').replace('\n', ' ')
+                    # remove mentions of The Web Conference 2022
                     pstring = pstring.replace("The Web Conference 2022", "")
                     pstring = pstring.replace("2022 World Wide Web Conference", "")
+                    pstring = pstring.replace("WWW ’22", "")
+                    pstring = pstring.replace("WWW 2022", "")
+                    pstring = pstring.replace("WWW, FR", "")
+                    pstring = pstring.replace("www ’22", "")
+                    pstring = pstring.replace("WWW, April 25–29, 2022, Lyon, France", "")
+                    pstring = pstring.replace("WWW ’2022", "")
+                    # remove URIs (to exclude http://www.)
+                    pstring = re.sub(r'http\S+', '', pstring)
+
+                    # lowercase, tokenise, remove punctuation
                     ptokens = tokenise(remove_punct(pstring))
-                    count = ptokens.count("web")
-                    print(pname, "mention of the term 'web':", count)
+
+                    count = 0
+                    count = count + ptokens.count("web")
+                    count = count + ptokens.count("www")
+
+#                    if (ptokens.count("web") and verbose):
+#                        index = ptokens.index("web")
+#                        print ("Single mention 'web':", ptokens[index-5:index+5])
+
+#                    if (ptokens.count("www") and verbose):
+#                        index = ptokens.index("www")
+#                        print ("Single mention 'www':", ptokens[index-5:index+5])
+
+                    if (verbose):
+                        print(pname, "mention of the term 'web' or 'www':", count)
+
+                    id = pname.replace("TheWebConf_2022_paper_", "")
+                    id = id.replace(".pdf", "")
+                    print(id, ",", count)
+
                     counter[count] = counter[count]+1
-                    if (count == 1 and verbose):
-                        index = ptokens.index("web")
-                        print ("Single mention:", ptokens[index-5:index+5])
                 except:
                     print("Error accessing", pname)
 
 print(counter.most_common())
-print("Of", papers, "papers, ", counter[0], "do not mention 'web' at all, ", counter[1], " once and ", counter[2], "twice")
+print("Of", papers, "papers, ", counter[0], "do not mention 'web' or 'www' at all, ", counter[1], " once, ", counter[2], "twice, ", counter[3], "three times")
